ea5718cb7cc1df77c4f7aa8e96c15ed135cd073f
infovalleysystemsolution <infovalleysystemsolution.com>
2024-02-20 23:10:06 -0300

    teste file_get_contents

--- routes.php
+++ routes.php
@@ -1,15 +1,15 @@
 <?php
 
-$response_data = ['error' => false, 'message' => '' , 'data' => null];
+// $response_data = ['error' => false, 'message' => '' , 'data' => null];
 
-// Obtém a URL da requisição
-// $request = $_SERVER['REQUEST_URI'];
+// // Obtém a URL da requisição
+// // $request = $_SERVER['REQUEST_URI'];
 
-$file_get_contents = json_decode(file_get_contents("php://input"), true);
+// $file_get_contents = json_decode(file_get_contents("php://input"), true);
 
-$array_data = ['file_get_contents' => $data /*, 'request' => $request */];
+// $array_data = ['file_get_contents' => $data /*, 'request' => $request */];
 
-$response_data = ['error' => false, 'message' => 'file_get_contents' , 'data' =>  $array_data ];
+// $response_data = ['error' => false, 'message' => 'file_get_contents' , 'data' =>  $array_data ];
 
-echo json_encode(['message' => 'PUT request processed', 'data' => $response_data]);
+// echo json_encode(['message' => 'PUT request processed', 'data' => $response_data]);
 
