b6655751ebf4823b7a055c3d8b8861c1bc2e1aa9
infovalleysystemsolution <infovalleysystemsolution.com>
2024-02-22 22:11:35 -0300

    teste file_get_contents

--- routes.php
+++ routes.php
@@ -18,8 +18,76 @@ $method = $_SERVER['REQUEST_METHOD'];
 // Obtém a URL da requisição
 $request = $_SERVER['REQUEST_URI'];
 
-// teste de comunicação
-echo json_encode(['message' => 'in index.php root', 'data' => ['method' => $method, 'request' => $request] ]);
+// Se o método for OPTIONS, é uma solicitação de preflight CORS, então não há necessidade de processar mais nada
+if ($method === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+// Processa a solicitação de acordo com o método
+if ($method === 'GET') {
+
+    // Lógica para lidar com solicitações GET
+    // echo json_encode(['message' => 'GET request processed']);
+
+    // Verifica se o parâmetro 'nome' foi passado na URL
+    if (isset($_GET['procurarcep'])) {
+        $cep = $_GET['procurarcep'];
+        // remover todos os caracteres na variável $cep que não forem numéricos
+        $cep = preg_replace("/[^0-9]/", "", $cep);
+
+        if (!is_numeric($cep) || $cep == "" || strlen($cep) != 8) {
+            echo "Digite um CEP válido (somente números e com 8 dígitos)";
+            return false;
+        }
+        
+        // $url = 'https://viacep.com.br/ws/' . $cep . '/json/';
+        // $ch = curl_init($url);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // $response = curl_exec($ch);
+        
+        // if($response === false){
+        //     echo "Ocorreu um erro ao executar a requisição: " . curl_error($ch);
+        // } else {
+        //     $data = json_decode($response, true);
+        //     var_dump($data);
+        //     $endereco = $data['logradouro'];
+        //     $bairro = $data['bairro'];
+        //     $cidade = $data['localidade'];
+        //     $estado = $data['uf'];
+        //     $cep = $data['cep'];
+        // }
+        
+        // curl_close($ch);
+        $response_data = ['error' => false, 'message' => '' , 'data' => $cep] ;
+
+        echo json_encode($response_data);
+    } else {
+        echo "Parâmetro 'procurarcep' não encontrado na URL";
+    }
+
+
+} elseif ($method === 'POST') {
+    // Lógica para lidar com solicitações POST
+    $data = json_decode(file_get_contents("php://input"), true);
+    echo json_encode(['message' => 'POST request processed', 'data' => $data]);
+} elseif ($method === 'PUT') {
+    // Lógica para lidar com solicitações PUT
+    $data = json_decode(file_get_contents("php://input"), true);
+    echo json_encode(['message' => 'PUT request processed', 'data' => $data]);
+} elseif ($method === 'DELETE') {
+    // Lógica para lidar com solicitações DELETE
+    echo json_encode(['message' => 'DELETE request processed']);
+} else {
+    // Método não suportado
+    http_response_code(405);
+    echo json_encode(['error' => 'Method Not Allowed']);
+}
+
+
+
+
+
 
 // $response_data = ['error' => false, 'message' => '' , 'data' => null];
 
